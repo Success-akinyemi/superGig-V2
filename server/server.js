c7ff8bcba04b4cefd3931104c884057f0d39fd8f
Success-akinyemi <brownsamuel915@gmail.com>
2024-04-07 14:16:04 -0700

    server-cors-fixed

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ app.use(express.json())
 app.use(cookieParser())
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', `${process.env.CLIENT_URL}`);
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Credentials', true);
