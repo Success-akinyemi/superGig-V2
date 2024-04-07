355c0584de9cfea8c4a9fed36ede7e70bf920641
Success-akinyemi <brownsamuel915@gmail.com>
2024-04-07 15:48:58 -0700

    server-cors-fixed

--- server/server.js
+++ server/server.js
@@ -13,16 +13,15 @@ const app = express()
 
 app.use(express.json())
 app.use(cookieParser())
-
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', `${process.env.CLIENT_URL}`);
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  next();
+    res.header('Access-Control-Allow-Origin', `${process.env.CLIENT_URL}`);
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
   });
-  
-  const corsOptions = {
+
+const corsOptions = {
     origin: `${process.env.CLIENT_URL}`,
     credentials: true,
 };
