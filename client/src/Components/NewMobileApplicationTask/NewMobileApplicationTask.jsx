6804be85bdcfcfb69a9e6c45a632d04adb60144a
Success-akinyemi <brownsamuel915@gmail.com>
2024-05-27 12:46:38 -0700

    mobile application task for admin bug fix

--- client/src/Components/NewMobileApplicationTask/NewMobileApplicationTask.jsx
+++ client/src/Components/NewMobileApplicationTask/NewMobileApplicationTask.jsx
@@ -1,4 +1,4 @@
-import './createMobileApplicationTask.css'
+import './NewMobileApplicationTask.css'
 import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { useFetchAllMobileApplicationPlatform } from '../../hooks/fetch.hooks';
